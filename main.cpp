36b5d5d44816550ec6f028c91e58df04eae18cf3
dickxumamba <xuruoyu27@gmail.com>
2025-10-27 02:16:23 +0800

    updated

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <list>
+#include <string>            
 #include "Goat.h"
 using namespace std;
 
@@ -29,15 +30,27 @@ int main() {
     while (fin1 >> colors[i++]);
     fin1.close();
 
-    //==================test================================
-    Goat g1("Vida");                    
-    Goat g2("Nola", 7);                 
-    Goat g3("Todd", 12, "Red");         
-
-    cout << g1.get_name() << " (" << g1.get_age() << ", " << g1.get_color() << ")" <<endl;
-    cout << g2.get_name() << " (" << g2.get_age() << ", " << g2.get_color() << ")" <<endl;
-    cout << g3.get_name() << " (" << g3.get_age() << ", " << g3.get_color() << ")" <<endl;
+    
 
     return 0;
 }
 
+int select_goat(list<Goat> trip){
+
+}
+
+void delete_goat(list<Goat> &trip){
+
+}
+
+void add_goat(list<Goat> &trip, string [], string []){
+
+}
+
+void display_trip(list<Goat> trip){
+
+}
+
+int main_menu(){
+    
+}
